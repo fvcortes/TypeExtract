0bf60322bfea26a3b15b975c34c6e1b74b2c55e2
Felipe Cortes <felipe.cortes@sensedia.com>
2022-11-26 22:56:29 -0300

    Add map and fold functions for array types

--- Type.lua
+++ Type.lua
@@ -29,12 +29,28 @@ local numberType =
         float = { number = "number", integer = "number", float = "float" }
     }
 
-local function getTableTag(table)
-    local tablesize = #table
+function mapTable(tb,f)
+    local result = {}
+    for k,v in pairs(tb) do
+        result[k] = f(v)
+    end
+    return result
+end
+
+function foldTable(tb,f)
+    local result = tb[1]
+    for i = 2, #tb do
+        result = f(result, tb[i])
+    end
+    return result
+end
+
+local function getTableTag(tb)
+    local tablesize = #tb
     if (tablesize > 0) then
         return "array"
     else                        
-        local i,v = next(table)
+        local i,v = next(tb)
         if (i == nil) then
             return "empty"
         else
@@ -86,9 +102,10 @@ local function addArrayType(type1,type2)
     return {tag = "array", arrayType = type2}
 end
 
-local function addRecordType(type1,type2)
+local function addRecordType(r1,r2)
     return {tag = "record", recordType = type2}
 end
+
 local function addPrimitiveType(type1, type2)
     -- types are compatible and primitives
     if (isNumber(type1) and isNumber(type2)) then
@@ -96,16 +113,16 @@ local function addPrimitiveType(type1, type2)
     end
     return (type1 or type2)
 end
+
 function addType(type1, type2)
     -- TODO: add types
-    local result
     if(isCompatible(type1, type2)) then
         local tag = type1.tag
         if(tag == "array") then
-            return addArrayType(type1.arrayType, type2.arrayType)
+            return {tag = "array", arrayType = addArrayType(type1.arrayType, type2.arrayType)}
         else
             if (tag == "record") then
-                return addRecordType(type1.recordType,type2.recordType)
+                return {tag = "record", recordType = addRecordType(type1.recordType,type2.recordType)}
             else
                 return addPrimitiveType(type1,type2)
             end
@@ -116,12 +133,9 @@ function addType(type1, type2)
 end
 
 local function getArrayType(array)
-    -- TODO:
-    -- arrayOfTypes = map(array, getType)
-    -- fold(arrayOfTypes, addType)
     print("getArrayType")
-    local t = getType(array[1])
-    return t
+    arrayOfTypes = mapTable(array, getType)
+    return foldTable(arrayOfTypes, addType)
 end
 
 local function getRecordType(record)
--- tmp.lua
+++ tmp.lua
@@ -12,11 +12,14 @@ end
 
 
 
-hello(1,2,3)
-hello(1.0, 2, 3)
-hello(1, 2, 3.0)
-hello("", 2.0, 3)
---hello(1,'b',{a=1,b={z = {}}})
+--hello(1,2,3)
+--hello({1,2,3,4,3})
+hello({a = {1,2,3}, b = true, c = {x = {1,2,3.0}}})
+--hello(1.0, 2, 3)
+--hello(1, 2, 3.0)
+--hello(1.0, 2, 3)
+--hello("", 2.0, 3)
+--hello(1,'b',{a=1,b={z = {1,2,3.0}}})
 --hello(1,'b', "")
 --hello('a', {}, nil)
 hey()
